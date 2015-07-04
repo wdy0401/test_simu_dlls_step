9a3a04a452609975f4e28167ac1451eda13d1a48
wdy0401 <wdy0401@gmail.com>
2015-07-04 17:54:50 +0800

    用log（p2/p1）  替换到 p1

--- optionf/optionf.cpp
+++ optionf/optionf.cpp
@@ -1,26 +1,32 @@
 #include "optionf.h"
-#include"math.h"
+#include "math.h"
 #include "../../gpp_qt/wfunction/wmath.h"
 
 using namespace std;\
 
 optionf::optionf()
 {
+    inited=false;
 }
 
 void optionf::set_vol(long second,double price)
 {
-    vol_map[second]=price;
+    if(inited==false)
+    {
+        lastp=price;
+        inited=true;
+        return;
+    }
+    vol_map[second]=log(price)-log(lastp);
     vol_list.push_front(price);
 
 
     map <long,map<long,double>*>::iterator iter=vol_snap_map.begin();
     while(iter!=vol_snap_map.end())
     {
-        if((*(iter->second)).find(second/iter->first)==(*(iter->second)).end())
+        if((*(iter->second)).rbegin()->first/iter->first==second/iter->first)
         {
-            (*iter->second)[second/iter->first]=price;
-            vol_snap_list.find(iter->first)->second->push_front(price);
+            (*iter->second)[second]=price;
         }
     }
 }
@@ -30,18 +36,22 @@ double optionf::get_vol()
 }
 double optionf::get_vol_adjust()
 {
-    map<long,double>::iterator it1=vol_map.begin();
+    return get_vol_adjust(&vol_map);
+}
+double optionf::get_vol_adjust(std::map<long, double> * now_vol_map)
+{
+    map<long,double>::iterator it1=now_vol_map->begin();
     map<long,double>::iterator it2=it1;it2++;
-    if(it2==vol_map.end())
+    if(it2==now_vol_map->end())
     {
         return 0;
     }
     long time_length=0;
     double sum_vol=0;
     double sum_ave=0;
-    while(it1!=vol_map.end())
+    while(it1!=now_vol_map->end())
     {
-        if(it2!=vol_map.end())
+        if(it2!=now_vol_map->end())
         {
             long time_dur=it2->first-it1->first;
             sum_vol+=(time_dur-1)*(it2->second+it1->second)*(it2->second+it1->second)/4 + it1->second*it1->second;
@@ -63,27 +73,31 @@ double optionf::get_vol(long seconds)
     map <long,map<long,double>*>::iterator iter=vol_snap_map.find(seconds);
     if(iter!=vol_snap_map.end())
     {
-        return wmath::stdev(vol_snap_list.find(seconds)->second);
+        return get_vol_adjust(iter->second);
     }
     else
     {
         map<long,double> * new_map=new map<long,double>;
-        list<double> * new_list=new list<double>;
         vol_snap_map[seconds]=new_map;
-        vol_snap_list[seconds]=new_list;
         map<long,double>::iterator it=vol_map.begin();
+        long lastsec;
         while(it!=vol_map.end())
         {
             long sec=it->first;
             double value=it->second;
-            if(new_map->find(sec/seconds)==new_map->end())
+            if(new_map->size()==0)
             {
-                (*new_map)[sec/seconds]=value;
-                new_list->push_back(value);
+                (*new_map)[sec]=value;
+                lastsec=sec/seconds;
+            }
+            else if(lastsec!=sec/seconds)
+            {
+                (*new_map)[sec]=value;
+                lastsec=sec/seconds;
             }
             it++;
         }
+        return get_vol_adjust(new_map);
     }
-    return wmath::stdev(vol_snap_list.find(seconds)->second);
 }
 
--- optionf/optionf.h
+++ optionf/optionf.h
@@ -14,15 +14,17 @@ public:
     optionf();
     void set_vol(long second,double price);
     double get_vol();
+    double get_vol_adjust(std::map <long,double> *);
     double get_vol_adjust();
     double get_vol(long);
 
 
 private:
+    double lastp;
+    bool inited;
     std::list <double> vol_list;
     std::map <long,double> vol_map;
     std::map <long,std::map<long,double>*> vol_snap_map;//use open as bar value
-    std::map <long,std::list<double>*> vol_snap_list;//use open as bar value
 };
 //
 
